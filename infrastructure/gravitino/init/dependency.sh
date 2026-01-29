2e2fe5bdfea3531d1e5095fca9ce877d904a2240
Le Dinh Duc <ledinhduc1879@gmail.com>
2026-01-29 22:36:30 +0700

    Fix hadoop common for gravitino

--- infrastructure/gravitino/init/dependency.sh
+++ infrastructure/gravitino/init/dependency.sh
@@ -13,11 +13,20 @@ if [[ ! -d "${target_dir}/packages" ]]; then
 fi
 
 # =======================================================================
+# Download Hadoop Common JAR and MD5
+# Why?: This is needed as a dependency for hadoop-aws (provides AuditSpanSource and other classes)
+# =======================================================================
+HADOOP_COMMON_JAR="https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-common/${HADOOP_AWS_JAR_VERSION}/hadoop-common-${HADOOP_AWS_JAR_VERSION}.jar"
+echo "Downloading hadoop-common ${HADOOP_AWS_JAR_VERSION}"
+HADOOP_COMMON_MD5="${HADOOP_COMMON_JAR}.md5"
+download_and_verify "${HADOOP_COMMON_JAR}" "${HADOOP_COMMON_MD5}" ${target_dir}
+
+# =======================================================================
 # Download Hadoop AWS JAR and MD5
 # Why?: This is needed for Gravitino to connect to S3 compatible storage like MinIO
 # =======================================================================
 HADOOP_AWS_JAR="https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/${HADOOP_AWS_JAR_VERSION}/hadoop-aws-${HADOOP_AWS_JAR_VERSION}.jar"
-echo "Downloading ${HADOOP_AWS_JAR_VERSION}"
+echo "Downloading hadoop-aws ${HADOOP_AWS_JAR_VERSION}"
 HADOOP_AWS_MD5="${HADOOP_AWS_JAR}.md5"
 download_and_verify "${HADOOP_AWS_JAR}" "${HADOOP_AWS_MD5}" ${target_dir}
 
@@ -37,3 +46,14 @@ download_and_verify "${AWS_JAVA_SDK_BUNDLE_JAR}" "${AWS_JAVA_SDK_BUNDLE_MD5}" ${
 ICEBERG_AWS_JAR="https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-aws/${ICEBERG_VERSION}/iceberg-aws-${ICEBERG_VERSION}.jar"
 ICEBERG_AWS_MD5="${ICEBERG_AWS_JAR}.md5"
 download_and_verify "${ICEBERG_AWS_JAR}" "${ICEBERG_AWS_MD5}" ${target_dir}
+
+# =======================================================================
+# Download AWS SDK v2 Bundle JAR
+# Why?: Required by Iceberg AWS for S3FileIO with modern AWS SDK
+# =======================================================================
+echo "Downloading AWS SDK v2 Bundle ${AWS_SDK_V2_VERSION}"
+AWS_SDK_V2_BUNDLE_JAR="https://repo1.maven.org/maven2/software/amazon/awssdk/bundle/${AWS_SDK_V2_VERSION}/bundle-${AWS_SDK_V2_VERSION}.jar"
+wget -q "${AWS_SDK_V2_BUNDLE_JAR}" -P "${target_dir}/packages"
+
+AWS_SDK_V2_URL_CLIENT_JAR="https://repo1.maven.org/maven2/software/amazon/awssdk/url-connection-client/${AWS_SDK_V2_VERSION}/url-connection-client-${AWS_SDK_V2_VERSION}.jar"
+wget -q "${AWS_SDK_V2_URL_CLIENT_JAR}" -P "${target_dir}/packages"
